5614ab3ba4e34a25961ea4e3ac7acc5bf2dc26ed
Shahid Gillani <shahid5ssg@gmail.com>
2024-04-15 22:16:04 +0500

    fix flows page

--- app/(dashboard)/dashboard/page.tsx
+++ app/(dashboard)/dashboard/page.tsx
@@ -50,7 +50,7 @@ export default function DashboardPage() {
   return (
     <div className="grid min-h-screen w-full md:grid-cols-[220px_1fr] lg:grid-cols-[280px_1fr]">
       <div className="hidden border-r bg-muted/40 md:block">
-        <div className="flex h-full max-h-screen flex-col gap-2">
+        <div className="fixed flex h-full max-h-screen flex-col gap-2">
           <div className="flex h-14 items-center border-b px-4 lg:h-[60px] lg:px-6">
             <Link href="/" className="flex items-center gap-2 font-semibold">
               <Package2 className="h-6 w-6" />
@@ -61,7 +61,7 @@ export default function DashboardPage() {
               <span className="sr-only">Toggle notifications</span>
             </Button>
           </div>
-          <div className="flex-1">
+          <div className="flex-1 overflow-y-auto overflow-x-hidden">
             <nav className="grid items-start px-2 text-sm font-medium lg:px-4">
               <form>
                 <div className="relative">
@@ -74,15 +74,13 @@ export default function DashboardPage() {
                 </div>
               </form>
               <Separator className="my-4" />
-              <Link
-                href="#"
-                className="flex items-center gap-3 rounded-lg px-3 py-2 text-muted-foreground transition-all hover:text-primary"
+              <div
+                className="flex items-center gap-3 rounded-lg px-3 py-2 text-muted-foreground"
               >
                 <ChevronDown className="h-4 w-4" />
                 {/* <User className="h-4 w-4" /> */}
                 Your Workspace
-              </Link>
-
+              </div>
               {/* <Link
                 href="#"
                 className="flex items-center gap-3 rounded-lg px-3 py-2 text-muted-foreground transition-all hover:text-primary"
@@ -122,6 +120,7 @@ export default function DashboardPage() {
         </div>
       </div>
       <div className="flex flex-col">
+        <div className="sticky top-0">
         <header className="flex h-14 items-center gap-4 border-b bg-muted/40 px-4 lg:h-[60px] lg:px-6">
           <Sheet>
             <SheetTrigger asChild>
@@ -228,6 +227,8 @@ export default function DashboardPage() {
             </DropdownMenuContent>
           </DropdownMenu>
         </header>
+        </div>
+
         <main className="flex flex-1 flex-col p-4 lg:p-6">
           <div className="flex items-center">
             <h1 className="text-lg font-semibold md:text-lg">My workspace</h1>
