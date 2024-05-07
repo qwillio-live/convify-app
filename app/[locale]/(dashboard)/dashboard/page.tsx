3b22a3ee627697b62ca4fe0ed7f93d0f41f6c4ba
Habibur Rahman <hrsshainik@gmail.com>
2024-05-07 21:06:05 +0600

    add avatar icon as fallback of user name first char

--- app/[locale]/(dashboard)/dashboard/page.tsx
+++ app/[locale]/(dashboard)/dashboard/page.tsx
@@ -280,7 +280,26 @@ export default function DashboardPage() {
                   size="sm"
                   className="!size-10 rounded-full bg-[#eaeaec] text-base font-bold hover:bg-[#eaeaec]"
                 >
-                  {userData && userData?.name.charAt(0).toUpperCase()}
+                  {userData && userData?.name ? (
+                    userData?.name?.charAt(0).toUpperCase()
+                  ) : (
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="20"
+                      height="20"
+                      viewBox="0 0 24 24"
+                      fill="none"
+                      stroke="currentColor"
+                      strokeWidth="2"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      className="lucide lucide-circle-user"
+                    >
+                      <circle cx="12" cy="12" r="10" />
+                      <circle cx="12" cy="10" r="3" />
+                      <path d="M7 20.662V19a2 2 0 0 1 2-2h6a2 2 0 0 1 2 2v1.662" />
+                    </svg>
+                  )}
                   <span className="sr-only">Toggle user menu</span>
                 </Button>
               </DropdownMenuTrigger>
