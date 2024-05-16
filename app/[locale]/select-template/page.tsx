c99f70be212f00eb58f61d535e0ac1659ddd4f0b
Habibur Rahman <hrsshainik@gmail.com>
2024-05-17 03:53:57 +0600

    fix select template issue

--- app/[locale]/select-template/page.tsx
+++ app/[locale]/select-template/page.tsx
@@ -5,10 +5,10 @@ import SelectTemplate from "@/components/select-template"
 
 export default function SelectTemplatePage() {
   return (
-    <div className="pt-8 pb-6 px-6 lg:px-12 lg:py-20">
+    <div className="pt-8 pb-6 px-6 lg:py-12 lg:px-20">
       <div className="w-full lg:w-[80%] mx-auto">
         <div className="">
-          <Image src={logo} alt="logo" className="mb-4" />
+          <Image src={logo} alt="logo" className="mb-4 w-24" />
           <p className="text-xl text-gray-500 mb-4">Building your first flow</p>
           <h1 className="text-black text-4xl lg:text-5xl font-bold mb-4">
             Your ideal template to start with
