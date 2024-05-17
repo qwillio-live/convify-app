0f23d958ec59dde8df17d09a5ea234ac48f49c0a
Shahzaib Rasheed <chshahzaib006@gmail.com>
2024-05-18 03:21:27 +0500

    updated ui for Signup page

--- app/(auth)/register/page.tsx
+++ app/(auth)/register/page.tsx
@@ -4,6 +4,8 @@ import { cn } from "@/lib/utils"
 import { buttonVariants } from "@/components/ui/button"
 import { Icons } from "@/components/icons"
 import { UserRegForm } from "@/components/user-reg-form"
+import signupImage from "@/assets/images/signup.svg"
+import convifyBlue from "@/assets/images/convify_logo_blue.svg"
 
 export const metadata = {
   title: "Create an account",
@@ -13,6 +15,15 @@ export const metadata = {
 export default function RegisterPage() {
   return (
     <div className="container grid h-screen w-screen flex-col items-center justify-center lg:max-w-none lg:grid-cols-2 lg:px-0">
+      <div
+        className="hidden h-full flex-col items-center justify-center bg-muted p-8 lg:flex"
+        style={{
+          backgroundColor: "#0F172A",
+        }}
+      >
+        <img src={convifyBlue.src} alt="Convify Logo" className="mb-8 w-40" />
+        <img src={signupImage.src} alt="Signup" className="h-auto max-w-full" />
+      </div>
       <Link
         href="/login"
         className={cn(
@@ -22,16 +33,20 @@ export default function RegisterPage() {
       >
         Login
       </Link>
-      <div className="hidden h-full bg-muted lg:block" />
       <div className="lg:p-8">
         <div className="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px]">
-          <div className="flex flex-row justify-center text-center">
-            <Icons.logo className="mr-2 size-8" />
-            <h1 className="text-2xl font-semibold tracking-tight">Convify</h1>
+          <div className="mb-2 flex flex-col justify-center text-center">
+            <div className="flex justify-center">
+              <Icons.logo className="size-8" />
+            </div>
+            <h1 className="mt-2 text-center text-2xl font-semibold tracking-tight">
+              Create an account
+            </h1>
+            <p className="font-600 text-md mt-2 text-center text-muted-foreground">
+              Enter you email below to create your account
+            </p>
           </div>
-          <p className="font-600 text-center text-xl text-muted-foreground">
-            Welcome to Convify. Dirty up your hands
-          </p>
+
           <UserRegForm />
         </div>
       </div>
