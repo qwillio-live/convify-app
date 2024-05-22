36b9c8e0f0e3c3734c68063eb40d106ab51c42a3
Shahid Gillani <shahid5ssg@gmail.com>
2024-05-22 05:53:44 +0500

    removed old dashboard folder

--- components/flow-preview/flow-preview.component.tsx
+++ components/flow-preview/flow-preview.component.tsx
@@ -4,16 +4,16 @@ import ResolvedComponentsFromCraftState from '@/components/user/settings/resolve
 import { useAppSelector } from '@/lib/state/flows-state/hooks'
 
 export default function FlowPreview() {
-  const selectedScreenIdex = useAppSelector((state) => state.screen.selectedScreen);
-  const selectedScreen = useAppSelector((state) => state.screen.screens[selectedScreenIdex]);
-  const screens = useAppSelector((state) => state.screen.screens);
+  const selectedScreenIdex = useAppSelector((state) => state?.screen?.selectedScreen);
+  const selectedScreen = useAppSelector((state) => state?.screen?.screens[selectedScreenIdex || 0]);
+  const screens = useAppSelector((state) => state?.screen?.screens);
   React.useEffect(() => {
     console.log(selectedScreen)
   }, [selectedScreen])
   return(
     <>
     {
-      screens.map((screen, index) => {
+      screens?.map((screen, index) => {
         return(
           <div
           key={index}
