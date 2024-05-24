5d7b6e9c15abe7c23adb56ddfa94064c9844fa1e
Rahul Gusai <rgusai97@gmail.com>
2024-05-24 16:59:09 +0530

    Delete Flow endpoint

--- app/api/flows/[id]/route.ts
+++ app/api/flows/[id]/route.ts
@@ -3,13 +3,13 @@ import { logError } from "@/lib/utils/logger"
 import { authOptions } from "@/lib/auth"
 import { NextRequest, NextResponse } from "next/server"
 import prisma from "@/lib/prisma"
+import { func } from "prop-types"
 
 export async function PUT(
   req: NextRequest,
   { params }: { params: { id: string } }
 ) {
   const { id } = params
-
   const data = await getServerSession(authOptions)
 
   if (!data) {
@@ -42,6 +42,54 @@ export async function PUT(
     const updatedFlow = await prisma.flow.update({
       where: { id: String(id) },
       data,
+    })
+
+    return NextResponse.json(updatedFlow)
+  } catch (error) {
+    const statusCode = 500
+    const errorMessage = error.message || "An unexpected error occurred"
+    const requestUrl = req.url
+    await logError({ statusCode, errorMessage, userId, requestUrl })
+    return NextResponse.json({ error: errorMessage }, { status: statusCode })
+  }
+}
+
+export async function DELETE(
+  req: NextRequest,
+  { params }: { params: { id: string } }
+) {
+  const { id } = params
+  const data = await getServerSession(authOptions)
+
+  if (!data) {
+    const statusCode = 401
+    const errorMessage = "User is not authenticated"
+    const userId = "unknown"
+    const requestUrl = req.url
+    await logError({ statusCode, errorMessage, userId, requestUrl })
+    return NextResponse.json({ error: errorMessage }, { status: statusCode })
+  }
+
+  const userId = data.user.id
+  try {
+    const existingFlow = await prisma.flow.findFirst({
+      where: {
+        id: String(),
+        userId,
+      },
+    })
+
+    if (!existingFlow) {
+      const statusCode = 404
+      const errorMessage = "Flow not found"
+      const requestUrl = req.url
+      await logError({ statusCode, errorMessage, userId, requestUrl })
+      return NextResponse.json({ error: errorMessage }, { status: statusCode })
+    }
+
+    const updatedFlow = await prisma.flow.update({
+      where: { id: String(id) },
+      data: { isDeleted: true },
     })
 
     return NextResponse.json(updatedFlow)
