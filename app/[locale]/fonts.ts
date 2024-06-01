682ba3ed224ad23f801bb8bf2681be0beff4df3d
Khant Sithu <khantseithuu@gmail.com>
2024-06-01 09:55:06 +0630

    refactor: move fonts into dedicated file

---
+++ app/[locale]/fonts.ts
@@ -0,0 +1,409 @@
+import localFont from "next/font/local"
+import {
+  Inter,
+  Roboto_Mono,
+  Open_Sans,
+  Montserrat,
+  Lato,
+  Oswald,
+  Source_Sans_Pro,
+  Raleway,
+  PT_Sans,
+  Merriweather,
+  Nunito,
+  Playfair_Display,
+  Poppins,
+  Ubuntu,
+  Mukta,
+  Rubik,
+  Work_Sans,
+  Roboto_Condensed,
+  Noto_Sans,
+  Fira_Sans,
+  Quicksand,
+  Karla,
+  Cabin,
+  Barlow,
+  Arimo,
+  Teko,
+  Catamaran,
+  Libre_Franklin,
+  Oxygen,
+  Heebo,
+  Asap,
+  Bitter,
+  IBM_Plex_Sans,
+  Exo_2,
+  Dosis,
+  PT_Serif,
+  Overpass,
+  Varela_Round,
+  Questrial,
+  Inconsolata,
+  Rokkitt,
+  Red_Hat_Display,
+  Cairo,
+  Lora,
+  Titillium_Web,
+  Bebas_Neue,
+  Anton,
+  Zilla_Slab,
+  Nunito_Sans,
+  Roboto_Slab,
+} from "next/font/google"
+
+export const inter = Inter({
+  subsets: ["latin"],
+  variable: "--font-inter",
+  display: "swap",
+})
+
+export const roboto_mono = Roboto_Mono({
+  subsets: ["latin"],
+  variable: "--font-roboto-mono",
+  display: "swap",
+})
+
+// google fonts
+export const open_sans = Open_Sans({
+  subsets: ["latin"],
+  variable: "--font-open-sans",
+  display: "swap",
+})
+
+export const montserrat = Montserrat({
+  subsets: ["latin"],
+  variable: "--font-montserrat",
+  display: "swap",
+})
+
+export const lato = Lato({
+  subsets: ["latin"],
+  variable: "--font-lato",
+  display: "swap",
+  weight: "400",
+})
+
+export const oswald = Oswald({
+  subsets: ["latin"],
+  variable: "--font-oswald",
+  display: "swap",
+})
+
+export const source_sans_pro = Source_Sans_Pro({
+  subsets: ["latin"],
+  variable: "--font-source-sans-pro",
+  display: "swap",
+  weight: "400",
+})
+
+export const raleway = Raleway({
+  subsets: ["latin"],
+  variable: "--font-raleway",
+  display: "swap",
+})
+
+export const pt_sans = PT_Sans({
+  subsets: ["latin"],
+  variable: "--font-pt-sans",
+  display: "swap",
+  weight: "400",
+})
+
+export const merriweather = Merriweather({
+  subsets: ["latin"],
+  variable: "--font-merriweather",
+  display: "swap",
+  weight: "400",
+})
+
+export const nunito = Nunito({
+  subsets: ["latin"],
+  variable: "--font-nunito",
+  display: "swap",
+  weight: ["400", "700"],
+})
+
+export const playfair_display = Playfair_Display({
+  subsets: ["latin"],
+  variable: "--font-playfair-display",
+  display: "swap",
+})
+
+export const poppins = Poppins({
+  subsets: ["latin"],
+  variable: "--font-poppins",
+  display: "swap",
+  weight: ["400", "700"],
+})
+
+export const ubuntu = Ubuntu({
+  subsets: ["latin"],
+  variable: "--font-ubuntu",
+  display: "swap",
+  weight: ["400", "700"],
+})
+
+export const mukta = Mukta({
+  subsets: ["latin"],
+  variable: "--font-mukta",
+  display: "swap",
+  weight: ["400", "700"],
+})
+
+export const rubik = Rubik({
+  subsets: ["latin"],
+  variable: "--font-rubik",
+  display: "swap",
+  weight: ["400", "700"],
+})
+
+export const work_sans = Work_Sans({
+  subsets: ["latin"],
+  variable: "--font-work-sans",
+  display: "swap",
+  weight: ["400", "700"],
+})
+
+export const roboto_condensed = Roboto_Condensed({
+  subsets: ["latin"],
+  variable: "--font-roboto-condensed",
+  display: "swap",
+  weight: ["400", "700"],
+})
+
+export const noto_sans = Noto_Sans({
+  subsets: ["latin"],
+  variable: "--font-noto-sans",
+  display: "swap",
+  weight: ["400", "700"],
+})
+
+export const fira_sans = Fira_Sans({
+  subsets: ["latin"],
+  variable: "--font-fira-sans",
+  display: "swap",
+  weight: ["400", "700"],
+})
+
+export const quicksand = Quicksand({
+  subsets: ["latin"],
+  variable: "--font-quicksand",
+  display: "swap",
+  weight: ["400", "700"],
+})
+
+export const karla = Karla({
+  subsets: ["latin"],
+  variable: "--font-karla",
+  display: "swap",
+  weight: ["400", "700"],
+})
+
+export const cabin = Cabin({
+  subsets: ["latin"],
+  variable: "--font-cabin",
+  display: "swap",
+  weight: ["400", "700"],
+})
+
+export const barlow = Barlow({
+  subsets: ["latin"],
+  variable: "--font-barlow",
+  display: "swap",
+  weight: ["400", "700"],
+})
+
+export const arimo = Arimo({
+  subsets: ["latin"],
+  variable: "--font-arimo",
+  display: "swap",
+  weight: ["400", "700"],
+})
+
+export const teko = Teko({
+  subsets: ["latin"],
+  variable: "--font-teko",
+  display: "swap",
+  weight: ["400", "700"],
+})
+
+export const catamaran = Catamaran({
+  subsets: ["latin"],
+  variable: "--font-catamaran",
+  display: "swap",
+  weight: ["400", "700"],
+})
+
+export const libre_franklin = Libre_Franklin({
+  subsets: ["latin"],
+  variable: "--font-libre-franklin",
+  display: "swap",
+  weight: ["400", "700"],
+})
+
+export const oxygen = Oxygen({
+  subsets: ["latin"],
+  variable: "--font-oxygen",
+  display: "swap",
+  weight: ["400", "700"],
+})
+
+export const heebo = Heebo({
+  subsets: ["latin"],
+  variable: "--font-heebo",
+  display: "swap",
+  weight: ["400", "700"],
+})
+
+export const asap = Asap({
+  subsets: ["latin"],
+  variable: "--font-asap",
+  display: "swap",
+  weight: ["400", "700"],
+})
+
+export const bitter = Bitter({
+  subsets: ["latin"],
+  variable: "--font-bitter",
+  display: "swap",
+  weight: ["400", "700"],
+})
+
+export const ibm_plex_sans = IBM_Plex_Sans({
+  subsets: ["latin"],
+  variable: "--font-ibm-plex-sans",
+  display: "swap",
+  weight: ["400", "700"],
+})
+
+export const exo_2 = Exo_2({
+  subsets: ["latin"],
+  variable: "--font-exo-2",
+  display: "swap",
+  weight: ["400", "700"],
+})
+
+export const dosis = Dosis({
+  subsets: ["latin"],
+  variable: "--font-dosis",
+  display: "swap",
+  weight: ["400", "700"],
+})
+
+export const pt_serif = PT_Serif({
+  subsets: ["latin"],
+  variable: "--font-pt-serif",
+  display: "swap",
+  weight: ["400", "700"],
+})
+
+export const overpass = Overpass({
+  subsets: ["latin"],
+  variable: "--font-overpass",
+  display: "swap",
+  weight: ["400", "700"],
+})
+
+export const varela_round = Varela_Round({
+  subsets: ["latin"],
+  variable: "--font-varela-round",
+  display: "swap",
+  weight: "400",
+})
+
+export const questrial = Questrial({
+  subsets: ["latin"],
+  variable: "--font-questrial",
+  display: "swap",
+  weight: "400",
+})
+
+export const inconsolata = Inconsolata({
+  subsets: ["latin"],
+  variable: "--font-inconsolata",
+  display: "swap",
+  weight: "400",
+})
+
+export const rokkitt = Rokkitt({
+  subsets: ["latin"],
+  variable: "--font-rokkitt",
+  display: "swap",
+  weight: "400",
+})
+
+export const red_hat_display = Red_Hat_Display({
+  subsets: ["latin"],
+  variable: "--font-red-hat-display",
+  display: "swap",
+  weight: "400",
+})
+
+export const cairo = Cairo({
+  subsets: ["latin"],
+  variable: "--font-cairo",
+  display: "swap",
+  weight: "400",
+})
+
+export const lora = Lora({
+  subsets: ["latin"],
+  variable: "--font-lora",
+  display: "swap",
+  weight: "400",
+})
+
+export const titillium_web = Titillium_Web({
+  subsets: ["latin"],
+  variable: "--font-titillium-web",
+  display: "swap",
+  weight: "400",
+})
+
+export const bebas_neue = Bebas_Neue({
+  subsets: ["latin"],
+  variable: "--font-bebas-neue",
+  display: "swap",
+  weight: "400",
+})
+
+export const anton = Anton({
+  subsets: ["latin"],
+  variable: "--font-anton",
+  display: "swap",
+  weight: "400",
+})
+
+export const zilla_slab = Zilla_Slab({
+  subsets: ["latin"],
+  variable: "--font-zilla-slab",
+  display: "swap",
+  weight: "400",
+})
+
+export const nunito_sans = Nunito_Sans({
+  subsets: ["latin"],
+  variable: "--font-nunito-sans",
+  display: "swap",
+  weight: "400",
+})
+
+export const roboto_slab = Roboto_Slab({
+  subsets: ["latin"],
+  variable: "--font-roboto-slab",
+  display: "swap",
+  weight: "400",
+})
+
+// local fonts
+export const fontSans = localFont({
+  src: "../../assets/fonts/Inter-Regular.ttf",
+  variable: "--font-sans",
+})
+
+// Font files can be colocated inside of `pages`
+export const fontHeading = localFont({
+  src: "../../assets/fonts/CalSans-SemiBold.woff2",
+  variable: "--font-heading",
+})
