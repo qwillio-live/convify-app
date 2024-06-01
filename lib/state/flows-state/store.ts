0fe70482beff06063b1cf803111b7a28854eaffa
Khant Sithu <khantseithuu@gmail.com>
2024-06-01 09:31:57 +0630

    chore: Update redux-persist storage implementation

--- lib/state/flows-state/store.ts
+++ lib/state/flows-state/store.ts
@@ -1,42 +1,40 @@
-import { configureStore,combineReducers } from '@reduxjs/toolkit';
-import { persistStore, persistReducer } from 'redux-persist';
-import storage from 'redux-persist/lib/storage';
-import createSagaMiddleware from 'redux-saga';
-import rootSaga from './features/sagas/rootSaga';
-import screensReducer from './features/placeholderScreensSlice';
-import themeReducer from './features/theme/globalThemeSlice';
+import { configureStore, combineReducers } from "@reduxjs/toolkit"
+import { persistStore, persistReducer } from "redux-persist"
+import createSagaMiddleware from "redux-saga"
+import rootSaga from "./features/sagas/rootSaga"
+import screensReducer from "./features/placeholderScreensSlice"
+import themeReducer from "./features/theme/globalThemeSlice"
+import storage from "@/lib/redux-persist-storage"
 
-const sagaMiddleware = createSagaMiddleware();
+const sagaMiddleware = createSagaMiddleware()
 
 const persistConfig = {
-  key: 'root',
+  key: "root",
   storage,
-};
+}
 const rootReducer = combineReducers({
   screen: screensReducer,
   theme: themeReducer,
-});
+})
 
-const persistedReducer = persistReducer(persistConfig, rootReducer);
+const persistedReducer = persistReducer(persistConfig, rootReducer)
 
 export const makeStore = () => {
   const store = configureStore({
     middleware: (getDefaultMiddleware) =>
-      getDefaultMiddleware(
-        {
-          serializableCheck: {
-            ignoredActions: ['persist/PERSIST'],
-          },
-        }
-      ).concat(sagaMiddleware),
-    reducer:persistedReducer,
-  });
-  const persistor = persistStore(store);
-  sagaMiddleware.run(rootSaga);
-  return { store, persistor };
-};
+      getDefaultMiddleware({
+        serializableCheck: {
+          ignoredActions: ["persist/PERSIST"],
+        },
+      }).concat(sagaMiddleware),
+    reducer: persistedReducer,
+  })
+  const persistor = persistStore(store)
+  sagaMiddleware.run(rootSaga)
+  return { store, persistor }
+}
 
-export type AppStore = ReturnType<typeof makeStore>['store'];
-export type AppPersistor = ReturnType<typeof makeStore>['persistor'];
-export type RootState = ReturnType<AppStore['getState']>;
-export type AppDispatch = AppStore['dispatch'];
+export type AppStore = ReturnType<typeof makeStore>["store"]
+export type AppPersistor = ReturnType<typeof makeStore>["persistor"]
+export type RootState = ReturnType<AppStore["getState"]>
+export type AppDispatch = AppStore["dispatch"]
