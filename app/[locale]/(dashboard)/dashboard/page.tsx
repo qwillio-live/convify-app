0bb133bf9c4e0e088a28e29068ae4943966ce26a
Habibur Rahman <hrsshainik@gmail.com>
2024-06-08 04:20:17 +0600

    fix user avatar issue

--- app/[locale]/(dashboard)/dashboard/page.tsx
+++ app/[locale]/(dashboard)/dashboard/page.tsx
@@ -336,9 +336,30 @@ export default function DashboardPage() {
                   size="sm"
                   className="flex w-10 h-10 items-center justify-center rounded-full bg-[#eaeaec] p-0 text-base font-bold hover:bg-[#eaeaec] uppercase"
                 >
-                  {userData && userData?.name
-                    ? userData?.name?.charAt(0).toUpperCase()
-                    : userData?.email?.charAt(0)}
+                  {userData ? (
+                    userData?.name ? (
+                      userData?.name?.charAt(0).toUpperCase()
+                    ) : (
+                      userData?.email?.charAt(0)
+                    )
+                  ) : (
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="20"
+                      height="20"
+                      viewBox="0 0 24 24"
+                      fill="none"
+                      stroke="currentColor"
+                      strokeWidth="2"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      className="lucide lucide-circle-user"
+                    >
+                      <circle cx="12" cy="12" r="10" />
+                      <circle cx="12" cy="10" r="3" />
+                      <path d="M7 20.662V19a2 2 0 0 1 2-2h6a2 2 0 0 1 2 2v1.662" />
+                    </svg>
+                  )}
                   <span className="sr-only">Toggle user menu</span>
                 </Button>
               </DropdownMenuTrigger>
