8de33c8217b8f50052e8f882b8118003b5045b2f
Xurshid97 <xurshedrabo@gmail.com>
2024-06-15 22:13:08 +0200

    headline part, requirements are applied as expected

--- components/user/headline-text/headline-text-settings.tsx
+++ components/user/headline-text/headline-text-settings.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react"
+import React, { useEffect, useState, useCallback } from "react"
 import { useNode } from "@/lib/craftjs"
 import ContentEditable from "react-contenteditable"
 
@@ -23,8 +23,19 @@ import { Slider } from "@/components/ui/slider"
 
 import { Input } from "@/components/ui/input"
 import { Controller } from "../settings/controller.component"
+import {
+  AlignLeft,
+  AlignCenter,
+  AlignRight,
+  MoveHorizontal,
+} from "lucide-react";
+import { Tabs, TabsList, TabsTrigger } from "@/components/custom-tabs";
+import { useTranslations } from "next-intl";
+import { debounce } from 'lodash';
+import { useAppSelector } from "@/lib/state/flows-state/hooks";
 
 export const HeadlineTextSettings = () => {
+  const t = useTranslations("Components");
   const {
     actions: { setProp },
     fontSize,
@@ -37,6 +48,9 @@ export const HeadlineTextSettings = () => {
     marginBottom,
     textColor,
     tagType,
+    // justifyContent,
+    containerBackground,
+    containerSize,
   } = useNode((node) => ({
     text: node.data.props.text,
     fontSize: node.data.props.fontSize,
@@ -48,8 +62,52 @@ export const HeadlineTextSettings = () => {
     marginBottom: node.data.props.marginBottom,
     textColor: node.data.props.textColor,
     tagType: node.data.props.tagType,
+    // justifyContent: node.data.props.justifyContent,
+    containerBackground: node.data.props.containerBackground,
+    containerSize: node.data.props.containerSize,
   }))
 
+  const [localMarginTop, setLocalMarginTop] = useState(marginTop.value);
+  useEffect(() => {
+    setLocalMarginTop(marginTop.value);
+  }, [marginTop]);
+
+  const [localMarginBottom, setLocalMarginBottom] = useState(marginBottom.value);
+  useEffect(() => {
+    setLocalMarginBottom(marginTop.value);
+  }, [marginBottom]);
+
+  const [localMarginRight, setLocalMarginRight] = useState(marginRight.value);
+  useEffect(() => {
+    setLocalMarginRight(marginRight.value);
+  }, [marginRight]);
+
+  const [localMarginLeft, setLocalMarginLeft] = useState(marginLeft.value);
+  useEffect(() => {
+    setLocalMarginLeft(marginLeft.value);
+  }, [marginLeft]);
+
+  const [localFontSize, setLocalFontSize] = useState(fontSize.value);
+  useEffect(() => {
+    setLocalFontSize(fontSize.value);
+  }, [fontSize]);
+
+  const debouncedSetProp = useCallback(
+    debounce((property, value) => {
+      setProp((props) => { props[property] = value }, 0);
+    }), [setProp]
+  );
+
+  const handlePropChangeDebounced = useCallback(
+    debounce((property, value) => {
+      setProp((props) => {
+        props[property] = value; // Update directly in the props
+      });
+    }, 1000),
+    [setProp]
+  );
+
+  const themeBackgroundColor = useAppSelector((state) => state?.theme?.general?.backgroundColor)
   return (
     <>
       <Accordion type="single" collapsible className="w-full">
@@ -82,22 +140,26 @@ export const HeadlineTextSettings = () => {
               </Select>
             </div>
             <div className="style-control flex flex-col gap-2 pb-4 pt-2">
-              <p className="text-md text-muted-foreground">Font Size</p>
-              <Input
-                type="number"
-                value={fontSize.value}
-                placeholder={fontSize.value}
+              <div className="flex justify-between items-center">
+                <p className="text-md text-muted-foreground">Font Size</p>
+                <span className="text-md text-muted-foreground">{localFontSize}px</span>
+              </div>
+              <Slider
+                value={[localFontSize]}
+                onValueChange={(value) => {
+                  setLocalFontSize(value[0])
+                  debouncedSetProp("fontSize", { value: value[0], isCustomized: true, globalStyled: false })
+                }}
+                min={12}
                 max={100}
-                min={0}
+                step={1}
                 className="w-full"
-                onChange={(e) =>
-                  setProp((props) => (props.fontSize.value = e.target.value), 1000)
-                }
               />
             </div>
             <div className="style-control flex flex-col gap-2 pb-4 pt-2">
               <p className="text-md text-muted-foreground">Font Weight</p>
               <Select
+                defaultValue={fontWeight.value}
                 onValueChange={(e) => {
                   setProp((props) => (props.fontWeight.value = e), 1000)
                 }}
@@ -119,7 +181,7 @@ export const HeadlineTextSettings = () => {
               </Select>
             </div>
 
-            <div className="style-control flex flex-col gap-2 pb-4 pt-2">
+            {/* <div className="style-control flex flex-col gap-2 pb-4 pt-2">
               <p className="text-md text-muted-foreground">Align</p>
               <RadioGroup
                 defaultValue="left"
@@ -140,10 +202,10 @@ export const HeadlineTextSettings = () => {
                   <Label htmlFor="r3">Right</Label>
                 </div>
               </RadioGroup>
-            </div>
+            </div> */}
           </AccordionContent>
         </AccordionItem>
-        <AccordionItem value="item-2">
+        {/* <AccordionItem value="item-2">
           <AccordionTrigger className="flex w-full basis-full flex-row flex-wrap justify-between p-2  hover:no-underline">
             <span className="text-sm font-medium">Margin </span>
           </AccordionTrigger>
@@ -201,7 +263,7 @@ export const HeadlineTextSettings = () => {
               />
             </div>
           </AccordionContent>
-        </AccordionItem>
+        </AccordionItem> */}
         <AccordionItem value="item-3">
           <AccordionTrigger className="flex w-full basis-full flex-row flex-wrap justify-between p-2 hover:no-underline">
             <span className="text-sm font-medium">Appearance</span>
@@ -214,6 +276,155 @@ export const HeadlineTextSettings = () => {
                 value={textColor.value}
                 onChange={(e) => {
                   setProp((props) => (props.textColor.value = e.target.value))
+                }}
+              />
+            </div>
+          </AccordionContent>
+        </AccordionItem>
+
+        <AccordionItem value="item-4">
+          <AccordionTrigger className="flex w-full basis-full flex-row flex-wrap justify-between p-2 hover:no-underline">
+            <span className="text-sm font-medium">Design</span>
+          </AccordionTrigger>
+          <AccordionContent className="flex flex-col gap-y-2 p-2">
+            <div className="style-control col-span-2 flex w-full grow-0 basis-full flex-col gap-2">
+              <p className="text-md text-muted-foreground">Align</p>
+              <Tabs
+                value={textAlign.value}
+                defaultValue={textAlign.value}
+                onValueChange={(e) => {
+                  setProp((props) => {
+                    props.textAlign.value = e;
+                  }, 1000);
+                }}
+                className="flex-1"
+              >
+                <TabsList className="w-full grid grid-cols-3">
+                  <TabsTrigger value="left"><AlignLeft /></TabsTrigger>
+                  <TabsTrigger value="center"><AlignCenter /></TabsTrigger>
+                  <TabsTrigger value="right"><AlignRight /></TabsTrigger>
+                </TabsList>
+              </Tabs>
+            </div>
+
+            <div className="flex flex-row items-center col-span-2 space-x-2">
+              <label
+                htmlFor="backgroundcolor"
+                className="text-md text-muted-foreground font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70 basis-2/3"
+              >
+                {t("Background Color")}
+              </label>
+              <Input
+                defaultValue={themeBackgroundColor}
+                value={containerBackground}
+                onChange={(e) => {
+                  debouncedSetProp("containerBackground", e.target.value)
+                }}
+                className="basis-1/3"
+                type={"color"}
+                id="backgroundcolor"
+              />
+            </div>
+          </AccordionContent>
+        </AccordionItem>
+
+        <AccordionItem value="item-5">
+          <AccordionTrigger className="flex w-full basis-full flex-row flex-wrap justify-between p-2 hover:no-underline">
+            <span className="text-sm font-medium">Spacing</span>
+          </AccordionTrigger>
+          <AccordionContent className="flex flex-col gap-y-2 p-2">
+            <div className="style-control col-span-2 flex w-full grow-0 basis-full flex-col gap-2">
+              <p className="text-md text-muted-foreground">Container Size</p>
+              <Tabs
+                value={containerSize}
+                defaultValue={containerSize}
+                onValueChange={(value) => {
+                  setProp((props) => (props.containerSize = value), 1000);
+                }}
+                className="flex-1"
+              >
+                <TabsList className="w-full grid grid-cols-4">
+                  <TabsTrigger value="small">{t("S")}</TabsTrigger>
+                  <TabsTrigger value="medium">{t("M")}</TabsTrigger>
+                  <TabsTrigger value="large">{t("L")}</TabsTrigger>
+                  <TabsTrigger value="full"><MoveHorizontal /></TabsTrigger>
+                </TabsList>
+              </Tabs>
+            </div>
+
+            <div className="style-control col-span-2 flex w-full grow-0 basis-full flex-col gap-2 items-start">
+              <div className="flex w-full basis-full flex-row items-center gap-2 justify-between">
+                <Label htmlFor="marginTop">{t("Top")}</Label>
+                <span className="w-12 rounded-md border border-transparent px-2 py-0.5 text-right text-sm text-muted-foreground hover:border-border">
+                  {marginTop.value}
+                </span>
+              </div>
+              <Slider
+                value={[localMarginTop]}
+                onValueChange={(value) => {
+                  setLocalMarginTop(value[0]); // Update local state
+                  handlePropChangeDebounced("marginTop", value[0]); // Debounced update
+                }}
+                min={0}
+                max={100}
+                step={1}
+                className="w-full"
+              />
+            </div>
+
+            <div className="style-control col-span-2 flex w-full grow-0 basis-full flex-col gap-2 items-start">
+              <div className="flex w-full basis-full flex-row items-center gap-2 justify-between">
+                <Label htmlFor="marginTop">{t("Bottom")}</Label>
+                <span className="w-12 rounded-md border border-transparent px-2 py-0.5 text-right text-sm text-muted-foreground hover:border-border">
+                  {marginBottom.value}
+                </span>
+              </div>
+              <Slider
+                value={[localMarginBottom]}
+                max={100}
+                min={0}
+                step={1}
+                onValueChange={(value) => {
+                  setLocalMarginBottom(value[0]); // Update local state
+                  handlePropChangeDebounced("marginBottom", value[0]); // Debounced update
+                }}
+              />
+            </div>
+
+            <div className="style-control col-span-2 flex w-full grow-0 basis-full flex-col gap-2 items-start">
+              <div className="flex w-full basis-full flex-row items-center gap-2 justify-between">
+                <Label htmlFor="marginTop">{t("Left")}</Label>
+                <span className="w-12 rounded-md border border-transparent px-2 py-0.5 text-right text-sm text-muted-foreground hover:border-border">
+                  {marginLeft.value}
+                </span>
+              </div>
+              <Slider
+                value={[localMarginLeft]}
+                max={100}
+                min={0}
+                step={1}
+                onValueChange={(value) => {
+                  setLocalMarginLeft(value[0]); // Update local state
+                  handlePropChangeDebounced("marginLeft", value[0]); // Debounced update
+                }}
+              />
+            </div>
+
+            <div className="style-control col-span-2 flex w-full grow-0 basis-full flex-col gap-2 items-start">
+              <div className="flex w-full basis-full flex-row items-center gap-2 justify-between">
+                <Label htmlFor="marginTop">{t("Right")}</Label>
+                <span className="w-12 rounded-md border border-transparent px-2 py-0.5 text-right text-sm text-muted-foreground hover:border-border">
+                  {marginRight.value}
+                </span>
+              </div>
+              <Slider
+                value={[localMarginRight]}
+                max={100}
+                min={0}
+                step={1}
+                onValueChange={(value) => {
+                  setLocalMarginRight(value[0]); // Update local state
+                  handlePropChangeDebounced("marginRight", value[0]); // Debounced update
                 }}
               />
             </div>
--- components/user/headline-text/headline-text.component.tsx
+++ components/user/headline-text/headline-text.component.tsx
@@ -27,6 +27,27 @@ import { HeadlineTextSettings } from "./headline-text-settings"
 import { useAppDispatch, useAppSelector } from "@/lib/state/flows-state/hooks"
 import { StyleProperty } from "../types/style.types"
 
+export enum ContainerTextSize {
+  small = "small",
+  medium = "medium",
+  large = "large",
+  full = "full",
+}
+
+const ContainerTextSizeValues = {
+  small: "300px",
+  medium: "376px",
+  large: "576px",
+  full: "100%",
+};
+
+const ContainerTextMobileSizeValues = {
+  small: "300px",
+  medium: "330px",
+  large: "360px",
+  full: "100%",
+};
+
 export const HeadlineTextGen = ({
   text,
   fontSize,
@@ -39,6 +60,7 @@ export const HeadlineTextGen = ({
   textColor,
   fontFamily,
   tagType,
+  containerBackground,
   ...props
 }) => {
   return (
@@ -59,6 +81,7 @@ export const HeadlineTextGen = ({
         color: `${textColor.value}`,
         lineHeight: `${fontSize.value}px`,
         minWidth: "max-content",
+        background: `${containerBackground}`,
       }}
     />
   )
@@ -76,6 +99,10 @@ export const HeadlineText = ({
   marginBottom,
   textColor,
   tagType,
+  fullWidth,
+  // justifyContent,
+  containerBackground,
+  containerSize,
   ...props
 }) => {
   const {
@@ -91,15 +118,15 @@ export const HeadlineText = ({
   const [editable, setEditable] = useState(false)
   const primaryFont = useAppSelector((state) => state.theme?.text?.primaryFont)
   const primaryTextColor = useAppSelector((state) => state.theme?.text?.primaryColor);
-
+  const mobileScreen = useAppSelector((state) => state.theme?.mobileScreen);
   useEffect(() => {
     setProp((props) => props.fontFamily.value = primaryFont, 200);
-   },
-  [primaryFont])
+  },
+    [primaryFont])
 
   useEffect(() => {
     setProp((props) => props.textColor.value = primaryTextColor, 200)
-  },[primaryTextColor])
+  }, [primaryTextColor])
 
   useEffect(() => {
     if (selected) {
@@ -108,12 +135,27 @@ export const HeadlineText = ({
 
     setEditable(false)
   }, [selected])
+
+  const getWidthValue = () => {
+    if (mobileScreen) {
+      return containerSize === 'full' ? '100%' : ContainerTextMobileSizeValues[containerSize];
+    } else {
+      return containerSize === 'full' ? '100%' : ContainerTextSizeValues[containerSize];
+    }
+  };
   return (
     <div
       className="relative"
       {...props}
       ref={(ref: any) => ref && connect(drag(ref))}
       onClick={() => selected && setEditable(true)}
+
+      style={{
+        width: "100%",
+        display: "flex",
+        justifyContent: "center",
+        background: `${containerBackground}`,
+      }}
     >
       {isHovered && <Controller nameOfComponent={"HEADLINE"} />}
       <ContentEditable
@@ -138,6 +180,9 @@ export const HeadlineText = ({
           marginTop: `${marginTop.value}px`,
           marginBottom: `${marginBottom.value}px`,
           color: `${textColor.value}`,
+          outline: "none",
+          background: `${containerBackground}`,
+          width: getWidthValue(),
           // color: `${primaryTextColor}`
         }}
       />
@@ -157,6 +202,8 @@ interface HeadlineTextProps {
   marginTop: StyleProperty;
   marginBottom: StyleProperty;
   tagType: string;
+  fullWidth: boolean;
+  containerSize: ContainerTextSize;
 }
 
 export const HeadlineTextDefaultProps: HeadlineTextProps = {
@@ -164,13 +211,15 @@ export const HeadlineTextDefaultProps: HeadlineTextProps = {
   fontSize: { value: 42, isCustomized: false, globalStyled: false },
   fontFamily: { value: "inherit", isCustomized: false, globalStyled: false },
   textColor: { value: "inherit", isCustomized: false, globalStyled: false },
-  fontWeight: { value: "600", isCustomized: false, globalStyled: false },
-  textAlign: { value: "left", isCustomized: false, globalStyled: false },
+  fontWeight: { value: "700", isCustomized: false, globalStyled: false },
+  textAlign: { value: "center", isCustomized: false, globalStyled: false },
   marginLeft: { value: 0, isCustomized: false, globalStyled: false },
   marginRight: { value: 0, isCustomized: false, globalStyled: false },
-  marginTop: { value: 0, isCustomized: false, globalStyled: false },
-  marginBottom: { value: 0, isCustomized: false, globalStyled: false },
-  tagType: "h1"
+  marginTop: { value: 20, isCustomized: false, globalStyled: false },
+  marginBottom: { value: 20, isCustomized: false, globalStyled: false },
+  tagType: "h1",
+  fullWidth: true,
+  containerSize: ContainerTextSize.medium,
 };
 
 
