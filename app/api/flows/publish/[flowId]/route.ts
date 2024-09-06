193251575a2c99e6f761d7bdc0b12d96a39e9bec
Milos <crazydev0409@gmail.com>
2024-09-06 11:18:06 -0400

    updated the directory path

--- app/api/flows/publish/[flowId]/route.ts
+++ app/api/flows/publish/[flowId]/route.ts
@@ -5,7 +5,7 @@ export async function POST(req: NextRequest, { params }: { params: { flowId: str
   const reqBody = await req.json();
   const { flowId } = params;
   const { html } = reqBody;
-  const filePath = path.join(process.cwd(), `public/flows/${flowId}.html`);
+  const filePath = path.join(process.cwd(), `.next/static/flows/${flowId}.html`);
   try {
     if (!fs.existsSync(path.dirname(filePath))) {
       fs.mkdirSync(path.dirname(filePath), { recursive: true });
