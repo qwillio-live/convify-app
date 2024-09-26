c7ce3d1af50f04b33326c5744d8d1521b7f8d63b
Hassan-mergestack <127212320+Hassan-mergestack@users.noreply.github.com>
2024-09-26 14:59:52 +0500

    tetsing

--- app/[locale]/(published-flow)/[flowId]/page.tsx
+++ app/[locale]/(published-flow)/[flowId]/page.tsx
@@ -1,4 +1,4 @@
-export const revalidate = 0
+export const dynamic = "force-dynamic"
 import React from "react"
 import FlowStateSetter from "../storeSetter"
 import { unstable_setRequestLocale } from "next-intl/server"
