a3345c1eab10834861c667551bc77781e1123f21
Hassan-mergestack <127212320+Hassan-mergestack@users.noreply.github.com>
2024-10-02 13:04:05 +0500

    upd

--- app/[locale]/(dashboard)/dashboard/thumbnailCronProvider.tsx
+++ app/[locale]/(dashboard)/dashboard/thumbnailCronProvider.tsx
@@ -9,57 +9,57 @@ export const ScreenshotsProvider = ({ children }) => {
   console.log("timeee", autoScreenshotTime)
 
   // // Function to fetch links from /api/cron and take screenshots
-  const fetchLinksAndTakeScreenshots = async () => {
-    try {
-      const response = await fetch("/api/flows")
-      const flows = await response.json()
-      const filteredFlows = flows.filter((flow) => {
-        const updatedAtTime = new Date(flow.updatedAt).getTime() // Parse updatedAt date
-        const thumbnailUpdatedAtTime = new Date(
-          flow?.thumbnail_updatedAt
-        ).getTime() // Parse thumbnail_updatedAt date
+  //   const fetchLinksAndTakeScreenshots = async () => {
+  //     try {
+  //       const response = await fetch("/api/flows")
+  //       const flows = await response.json()
+  //       const filteredFlows = flows.filter((flow) => {
+  //         const updatedAtTime = new Date(flow.updatedAt).getTime() // Parse updatedAt date
+  //         const thumbnailUpdatedAtTime = new Date(
+  //           flow?.thumbnail_updatedAt
+  //         ).getTime() // Parse thumbnail_updatedAt date
 
-        // Calculate the time difference in milliseconds
-        const timeDifferenceInMilliseconds = Math.abs(
-          thumbnailUpdatedAtTime - updatedAtTime
-        )
+  //         // Calculate the time difference in milliseconds
+  //         const timeDifferenceInMilliseconds = Math.abs(
+  //           thumbnailUpdatedAtTime - updatedAtTime
+  //         )
 
-        // Convert the difference to minutes
-        const timeDifferenceInMinutes =
-          timeDifferenceInMilliseconds / (1000 * 60)
+  //         // Convert the difference to minutes
+  //         const timeDifferenceInMinutes =
+  //           timeDifferenceInMilliseconds / (1000 * 60)
 
-        console.log("Difference in minutes:", timeDifferenceInMinutes)
+  //         console.log("Difference in minutes:", timeDifferenceInMinutes)
 
-        // Check if the difference is greater than or equal to 5 minutes
-        return timeDifferenceInMinutes >= 5
-      })
+  //         // Check if the difference is greater than or equal to 5 minutes
+  //         return timeDifferenceInMinutes >= 5
+  //       })
 
-      console.log("flowsss", flows, "filteredFlows", filteredFlows)
-      if (filteredFlows && filteredFlows.length > 0) {
-        for (let eachFlow of filteredFlows) {
-          const response = await fetch(`/api/cron?flowId=${eachFlow.id}`)
-          // Handle the response data if needed
-        }
-      }
-    } catch (error) {
-      console.error("Error fetching links or taking screenshots:", error)
-    }
-  }
+  //       console.log("flowsss", flows, "filteredFlows", filteredFlows)
+  //       if (filteredFlows && filteredFlows.length > 0) {
+  //         for (let eachFlow of filteredFlows) {
+  //           const response = await fetch(`/api/cron?flowId=${eachFlow.id}`)
+  //           // Handle the response data if needed
+  //         }
+  //       }
+  //     } catch (error) {
+  //       console.error("Error fetching links or taking screenshots:", error)
+  //     }
+  //   }
 
-  useEffect(() => {
-    // Trigger the screenshot functionality every minute
-    const interval = setInterval(
-      () => {
-        console.log("calling screenshot")
-        fetchLinksAndTakeScreenshots() // Call the function every interval
-      },
-      autoScreenshotTime,
-      10
-    )
+  //   useEffect(() => {
+  //     // Trigger the screenshot functionality every minute
+  //     const interval = setInterval(
+  //       () => {
+  //         console.log("calling screenshot")
+  //         fetchLinksAndTakeScreenshots() // Call the function every interval
+  //       },
+  //       autoScreenshotTime,
+  //       10
+  //     )
 
-    // Cleanup interval when component unmounts
-    return () => clearInterval(interval)
-  }, []) //
+  //     // Cleanup interval when component unmounts
+  //     return () => clearInterval(interval)
+  //   }, []) //
 
   return <>{children}</>
 }
--- app/api/cron/route.tsx
+++ app/api/cron/route.tsx
@@ -13,117 +13,118 @@ export async function GET(req: NextRequest) {
   console.log("Entered cron")
 
   try {
-    const searchParams = req.nextUrl.searchParams
-    const flowId = searchParams.get("flowId")
-    const session = await getServerSession(authOptions)
-    const cookie = cookies().getAll() // Get all cookies from the request
-    if (!session) {
-      return new Response("Unauthorized", { status: 403 })
-    }
-    console.log("flowId", flowId)
-    const requiredEntitiess = await prisma.flowStep.findMany({
-      where: {
-        isDeleted: false,
-        order: 0,
-        flowId: flowId ? flowId : "",
-      },
-    })
-    let requiredEntities =
-      requiredEntitiess.length > 0 ? requiredEntitiess[0] : null
+    // const searchParams = req.nextUrl.searchParams
+    // const flowId = searchParams.get("flowId")
+    // const session = await getServerSession(authOptions)
+    // const cookie = cookies().getAll() // Get all cookies from the request
+    // if (!session) {
+    //   return new Response("Unauthorized", { status: 403 })
+    // }
+    // console.log("flowId", flowId)
+    // const requiredEntitiess = await prisma.flowStep.findMany({
+    //   where: {
+    //     isDeleted: false,
+    //     order: 0,
+    //     flowId: flowId ? flowId : "",
+    //   },
+    // })
+    // let requiredEntities =
+    //   requiredEntitiess.length > 0 ? requiredEntitiess[0] : null
 
-    let updatedFlow: any = null
-    const fetchLinksAndTakeScreenshots = async () => {
-      try {
-        if (requiredEntities) {
-          revalidateFlow({ tag: "previewFlow" })
-          const browser = await chromium.launch() // Launch Playwright browser
-          const page = await browser.newPage()
+    // let updatedFlow: any = null
+    // const fetchLinksAndTakeScreenshots = async () => {
+    //   try {
+    //     if (requiredEntities) {
+    //       revalidateFlow({ tag: "previewFlow" })
+    //       const browser = await chromium.launch() // Launch Playwright browser
+    //       const page = await browser.newPage()
 
-          // Format cookies to include necessary fields like domain
-          const formattedCookies = cookie.map((c) => ({
-            name: c.name,
-            value: c.value,
-            domain: process.env.NEXT_PUBLIC_APP_URL?.replace(
-              "https://",
-              ""
-            ).replace("http://", ""),
-            path: "/",
-          }))
+    //       // Format cookies to include necessary fields like domain
+    //       const formattedCookies = cookie.map((c) => ({
+    //         name: c.name,
+    //         value: c.value,
+    //         domain: process.env.NEXT_PUBLIC_APP_URL?.replace(
+    //           "https://",
+    //           ""
+    //         ).replace("http://", ""),
+    //         path: "/",
+    //       }))
 
-          // Set cookies before navigating to the page
-          await page.context().addCookies(formattedCookies)
-          console.log(
-            "linkkkk",
-            `${process.env.NEXT_PUBLIC_APP_URL}/dashboard/preview-flow/${flowId}?screen=${requiredEntities.name}`
-          )
-          await page.goto(
-            `${process.env.NEXT_PUBLIC_APP_URL}/dashboard/preview-flow/${flowId}?screen=${requiredEntities.name}`,
-            {
-              waitUntil: "networkidle",
-            }
-          )
+    //       // Set cookies before navigating to the page
+    //       await page.context().addCookies(formattedCookies)
+    //       console.log(
+    //         "linkkkk",
+    //         `${process.env.NEXT_PUBLIC_APP_URL}/dashboard/preview-flow/${flowId}?screen=${requiredEntities.name}`
+    //       )
+    //       await page.goto(
+    //         `${process.env.NEXT_PUBLIC_APP_URL}/dashboard/preview-flow/${flowId}?screen=${requiredEntities.name}`,
+    //         {
+    //           waitUntil: "networkidle",
+    //         }
+    //       )
 
-          // Save screenshot as a buffer
-          const screenshotBuffer = await page.screenshot({
-            type: "png",
-            fullPage: true,
-          })
-          console.log("Screenshot captured for", requiredEntities.name)
+    //       // Save screenshot as a buffer
+    //       const screenshotBuffer = await page.screenshot({
+    //         type: "png",
+    //         fullPage: true,
+    //       })
+    //       console.log("Screenshot captured for", requiredEntities.name)
 
-          // Ensure the screenshot buffer is correctly handled as a Buffer
-          const formData = new FormData()
-          formData.append("image", Buffer.from(screenshotBuffer), {
-            filename: `${requiredEntities.name}.png`,
-            contentType: "image/png",
-          })
-          formData.append("bucket_name", "convify-images")
-          formData.append("sizes[0]", "100x100")
-          formData.append("sizes[1]", "200x200")
+    //       // Ensure the screenshot buffer is correctly handled as a Buffer
+    //       const formData = new FormData()
+    //       formData.append("image", Buffer.from(screenshotBuffer), {
+    //         filename: `${requiredEntities.name}.png`,
+    //         contentType: "image/png",
+    //       })
+    //       formData.append("bucket_name", "convify-images")
+    //       formData.append("sizes[0]", "100x100")
+    //       formData.append("sizes[1]", "200x200")
 
-          try {
-            const response = await axios.post(
-              "https://file-uploader.picreel.bid/upload",
-              formData,
-              {
-                headers: {
-                  ...formData.getHeaders(),
-                },
-              }
-            )
+    //       try {
+    //         const response = await axios.post(
+    //           "https://file-uploader.picreel.bid/upload",
+    //           formData,
+    //           {
+    //             headers: {
+    //               ...formData.getHeaders(),
+    //             },
+    //           }
+    //         )
 
-            console.log(
-              `Upload successful for ${requiredEntities.name}:`,
-              response.data.data.images
-            )
-            if (flowId && response.data.data?.images?.original)
-              updatedFlow = await prisma.flow.update({
-                where: {
-                  id: flowId,
-                },
-                data: {
-                  thumbnail_updatedAt: new Date(),
-                  previewImage: response.data.data?.images?.original,
-                },
-              })
-          } catch (uploadError) {
-            console.error(
-              `Failed to upload screenshot for ${requiredEntities.name}:`,
-              uploadError
-            )
-          }
-          await browser.close() // Close Playwright browser
-        }
-      } catch (error) {
-        console.error("Error fetching links or taking screenshots:", error)
-      }
-    }
+    //         console.log(
+    //           `Upload successful for ${requiredEntities.name}:`,
+    //           response.data.data.images
+    //         )
+    //         if (flowId && response.data.data?.images?.original)
+    //           updatedFlow = await prisma.flow.update({
+    //             where: {
+    //               id: flowId,
+    //             },
+    //             data: {
+    //               thumbnail_updatedAt: new Date(),
+    //               previewImage: response.data.data?.images?.original,
+    //             },
+    //           })
+    //       } catch (uploadError) {
+    //         console.error(
+    //           `Failed to upload screenshot for ${requiredEntities.name}:`,
+    //           uploadError
+    //         )
+    //       }
+    //       await browser.close() // Close Playwright browser
+    //     }
+    //   } catch (error) {
+    //     console.error("Error fetching links or taking screenshots:", error)
+    //   }
+    // }
 
-    // Execute screenshot function
-    await fetchLinksAndTakeScreenshots()
+    // // Execute screenshot function
+    // await fetchLinksAndTakeScreenshots()
 
-    return NextResponse.json({
-      flows: updatedFlow ? updatedFlow : requiredEntities?.name ?? "false",
-    })
+    // return NextResponse.json({
+    //   flows: updatedFlow ? updatedFlow : requiredEntities?.name ?? "false",
+    // })
+    return NextResponse.json({ status: true })
   } catch (error) {
     console.error("Error fetching flows:", error)
     const statusCode = 500
