aa0ef6d718d3b0ac7ba8732b5c88e8a5c8a2a91a
anjit pariyar <anjitpariyar@gmail.com>
2024-10-16 16:15:22 +0545

    fix: scroll bar

--- components/ui/scroll-area.tsx
+++ components/ui/scroll-area.tsx
@@ -46,7 +46,7 @@ const ScrollBar = React.forwardRef<
     )}
     {...props}
   >
-    <ScrollAreaPrimitive.ScrollAreaThumb className="bg-border relative flex-1 rounded-full" />
+    <ScrollAreaPrimitive.ScrollAreaThumb className="bg-border z-50 relative flex-1 rounded-full" />
   </ScrollAreaPrimitive.ScrollAreaScrollbar>
 ))
 ScrollBar.displayName = ScrollAreaPrimitive.ScrollAreaScrollbar.displayName
